f58aaed86bf0661207fdcf52463b98384fc8fe6b
unknown <rayansharara05@gmail.com>
2025-02-27 18:38:22 +1100

    Completed Chaos backend Question 1

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -3,9 +3,22 @@ use serde::{Deserialize, Serialize};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
+    let mut int_sum: i64 = 0;
+    let mut str_sum: usize = 0;
 
+    // Going through each element and incrementing the relevant variable
+    for item in request.data {
+        match item {
+            serde_json::Value::Number(num) => int_sum += num.as_i64().unwrap(),
+            serde_json::Value::String(str) => str_sum += str.len(),
+            _ => continue,
+        }
+    }
+
+    // Sorting and returning the results
     let response = DataResponse {
-        
+        string_len: str_sum,
+        int_sum: int_sum,
     };
 
     (StatusCode::OK, Json(response))
@@ -13,10 +26,11 @@ pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse
 
 #[derive(Deserialize)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<serde_json::Value>,
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: usize,
+    int_sum: i64,
 }
