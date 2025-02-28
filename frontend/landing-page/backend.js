4d08b9d3eef0867f0d877514e2460e5ab412b2e1
Rayan Sharara <64306192+LoneRanger9000@users.noreply.github.com>
2025-02-28 16:26:52 +1100

    Added functionality so that door can open or close when clicked

---
+++ frontend/landing-page/backend.js
@@ -0,0 +1,14 @@
+  const logoContainer = document.getElementById('freerooms-logo-container');
+const logoImg = document.getElementById('door-logo')
+
+let clickCounter = 0;
+
+logoContainer.addEventListener('click', () => {
+    clickCounter++;
+
+    if (clickCounter % 2 === 0) {
+        logoImg.src = '../assets/openDoor.png';
+    } else {
+        logoImg.src = '../assets/freeroomsDoorClosed.png';
+    }
+});
