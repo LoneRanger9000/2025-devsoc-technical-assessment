8a0904a980f80a052299db7c258e1c7233ccf119
Rayan Sharara <64306192+LoneRanger9000@users.noreply.github.com>
2025-03-01 14:02:03 +1100

    Completed backend task 3

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -96,11 +96,68 @@ app.post("/entry", (req:Request, res:Response) => {
 });
 
 // [TASK 3] ====================================================================
+// Helper function that allows us recurse into further ingredients
+const push_required_ingredients_to_stack = (recipeObject: recipe, numRecipe: number, stack: {name: string, quantity: number}[]) => {
+  for (let requiredItem of recipeObject.requiredItems) {
+    stack.push({name: requiredItem.name, quantity: requiredItem.quantity * numRecipe});
+  }
+}
+
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  // Reading in the name of the recipe
+  const { name } = req.query;
 
+  // Finding the entry in the cookbook
+  const targetEntry = cookbook.find(dish => dish.name === name);
+
+  // Error if not found or of the wrong type
+  if (!targetEntry || targetEntry.type != "recipe") {
+    res.status(400).send("Can't find or is not a recipe");
+  }
+
+  // stack and map will help us in compiling the list of all ingredients required
+  const ingredientsMap: Map<string, number> = new Map();
+  const stack: {name: string, quantity: number}[] = [];
+  push_required_ingredients_to_stack(targetEntry as recipe, 1, stack);
+
+  while (stack.length !== 0) {
+    const top = stack.pop();
+    const curName = top.name;
+    const quantity = top.quantity;
+    const curEntry = cookbook.find(entry => entry.name === curName);
+
+    // If required ingredient not found, then return error
+    if (!curEntry)
+      res.status(400).send("Can't find a requiredIngredient");
+
+    // if it is an ingredient, add it to the map
+    // if it is a recipe, continue recursing
+    if (curEntry.type === "ingredient") {
+      if (ingredientsMap.has(curName)) {
+        ingredientsMap.set(curName, ingredientsMap.get(curName) + quantity);
+      } else {
+        ingredientsMap.set(curName, quantity);
+      }
+    } else {
+      push_required_ingredients_to_stack(curEntry as recipe, quantity, stack);
+    }
+  }
+
+  // Compiling a summary
+  const summary: {name: string; cookTime: number; ingredients: {name: string; quantity: number}[]} = {
+    name: name,
+    cookTime: 0,
+    ingredients: []
+  };
+
+  // Traversing the map, and adding each ingredient
+  ingredientsMap.forEach((value, key) => {
+    summary.ingredients.push({name: key, quantity: value});
+    summary.cookTime += value * (cookbook.find(ingredient => ingredient.name == key) as ingredient).cookTime;
+  });
+
+  res.status(200).json(summary);
 });
 
 // =============================================================================
